a200b716118cf521e8808976d17220500e572d9d
Laura <lauj@teknologisk.dk>
2019-04-26 13:56:12 +0200

    fixed timestep function

--- code/TRPO/ur5_reacher.py
+++ code/TRPO/ur5_reacher.py
@@ -22,40 +22,39 @@ from senseact.utils import tf_set_seeds, NormalizedEnv
 from helper import create_callback
 from ur_setups import setup
 
+# Directory for evaluation logs
 parser = argparse.ArgumentParser(description='Benchmarking DRL on Real World Robots')
-parser.add_argument('--log_dir', type=str, default="../../logs/TRPO",
+parser.add_argument('--log_dir', type=str, default='../../logs/TRPO',
                     help='path to put log files')
 args = parser.parse_args()
 
 def main():
     # Set hyperparameters here
-    hid_size = 32              # 2^[3,X] where X is ?
-    num_hid_layers = 2         # [1,4]
-    timesteps_per_batch = 2048 # 2^[8,13]
-    vf_stepsize = 0.001        # 10^[-5,-2]
-    max_kl = 0.05              # 10^[-2.5,-0.5]
-    cg_iters = 10              # I don't know what this is in the table from SenseAct paper b, see trpo_mpi
-    cg_damping = 0.1           # I don't know what this is either see trpo_mpi
-    gamma = 0.995              # 1 - (1/(c_gamma*N)), where N = T/tau, T is the total length of an ep in time (?) and tau is the action cycle time
-    lam = 0.995                # 1 - (1/(c_lambda*N)), where N = T/tau, T is the total length of an ep in time (?) and tau is the action cycle time    
-
-    # use fixed random state
+    hid_size = 32
+    num_hid_layers = 2
+    timesteps_per_batch = 2048
+    vf_stepsize = 0.001
+    max_kl = 0.05
+    cg_iters = 10
+    cg_damping = 0.1
+    gamma = 0.995
+    lam = 0.995
+    
+    # Use fixed random state
     rand_state = np.random.RandomState(1).get_state()
     np.random.set_state(rand_state)
     tf_set_seeds(np.random.randint(1, 2**31 - 1))
 
     # Create UR5 Reacher2D environment
-    dt = 0.04 # Used to log data, therefore it is defined here
-
     env = ReacherEnv(
             setup=setup,
             host=None,
             dof=2,
-            control_type="velocity",
-            target_type="position",
-            reset_type="zero",
-            reward_type="precision",
-            derivative_type="none",
+            control_type='velocity',
+            target_type='position',
+            reset_type='zero',
+            reward_type='precision',
+            derivative_type='none',
             deriv_action_max=5,
             first_deriv_max=2,
             accel_max=1.4,
@@ -64,16 +63,18 @@ def main():
             episode_length_time=4.0,
             episode_length_step=None,
             actuation_sync_period=1,
-            dt=dt,
-            run_mode="multiprocess",
+            dt=0.04,
+            run_mode='multiprocess',
             rllab_box=False,
             movej_t=2.0,
             delay=0.0,
             random_state=rand_state
         )
     env = NormalizedEnv(env)
+
     # Start environment processes
     env.start()
+
     # Create baselines TRPO policy function
     sess = U.single_threaded_session()
     sess.__enter__()
@@ -81,11 +82,13 @@ def main():
         return MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
             hid_size=hid_size, num_hid_layers=num_hid_layers)
 
-    # CHANGES HERE!! Create and start logging process  CHANGES HERE!!
-    log_running = Value('i', 1) # flag
-    shared_returns = Manager().dict({"write_lock": False,
-                                     "episodic_returns": [],
-                                     "episodic_lengths": [], }) # A manager dictionary object containing `episodic returns` and `episodic lengths`
+    # Create and start logging process
+    log_running = Value('i', 1)
+    # Manager to share data between log process and main process
+    shared_returns = Manager().dict({'write_lock': False,
+                                     'episodic_returns': [],
+                                     'episodic_lengths': [], }) 
+    
     # Spawn logging process
     pp = Process(target=log_function, args=(env, timesteps_per_batch, shared_returns, log_running, args.log_dir))
     pp.start()
@@ -119,19 +122,20 @@ def log_function(env, batch_size, shared_returns, log_running, log_dir):
     Args:
 	env: An instance of ReacherEnv
         batch_size: An int representing timesteps_per_batch provided to the TRPO learn function
-        shared_returns: A manager dictionary object containing `episodic returns` and `episodic lengths`
+        shared_returns: A manager dictionary object containing 'episodic returns' and 'episodic lengths'
         log_running: A multiprocessing Value object containing 0/1 - a flag to allow logging while process is running
     """
 
     old_size = len(shared_returns['episodic_returns']) 
-    time.sleep(5.0) # Process sleep for 5 seconds
+    time.sleep(5.0)
     rets = []
 
+    # Create logs directory
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
     time_now = time.time()
-    file = open(os.path.join(os.path.join(log_dir, str(time_now)+".csv")), 'a')
+    file = open(os.path.join(os.path.join(log_dir, str(time_now)+'.csv')), 'a')
 
     file.write('Episode,Step,Reward,X-Target,Y-Target,Z-Target,X-Current,Y-Current,Z-Current \n') # Header with names for all values logged
 
@@ -146,13 +150,13 @@ def log_function(env, batch_size, shared_returns, log_running, log_dir):
             old_size = len(copied_returns['episodic_returns'])
 
             if len(rets):
-                file.write(str(episode) + "," + str(int(episode/0.04)) + "," + str(rets[-1]) + 
-                           "," + str(env._x_target_[2]) + "," + str(env._x_target_[1])  + "," + str(env._x_target_[0])
-                           + "," + str(env._x_[2]) + "," + str(env._x_[1]) + "," + str(env._x_[0]) + '\n')
+                file.write(str(episode) + ',' + str(int(episode*100) + ',' + str(rets[-1]) + 
+                           ',' + str(env._x_target_[2]) + ',' + str(env._x_target_[1])  + ',' + str(env._x_target_[0])
+                           + ',' + str(env._x_[2]) + ',' + str(env._x_[1]) + ',' + str(env._x_[0]) + '\n')
             else:
-                file.write(str(episode) + "," + str(int(episode/0.04)) + "," + 'NaN' + 
-                           "," + str(env._x_target_[2]) + "," + str(env._x_target_[1])  + "," + str(env._x_target_[0])
-                           + "," + str(env._x_[2]) + "," + str(env._x_[1]) + "," + str(env._x_[0]) + '\n')
+                file.write(str(episode) + ',' + str(int(episode*100)) + ',' + 'NaN' + 
+                           ',' + str(env._x_target_[2]) + ',' + str(env._x_target_[1])  + ',' + str(env._x_target_[0])
+                           + ',' + str(env._x_[2]) + ',' + str(env._x_[1]) + ',' + str(env._x_[0]) + '\n')
             
             # Calculate rolling average of rewards
             window_size_steps = 5000
