ef0002e6225653ac56df13221865b3410f48aad0
Nicolai Anton Lynnerup <nily@teknologisk.dk>
2019-05-13 22:22:16 +0200

    Added some PPO hyperparams

--- random/ppo/ur5_reacher.py
+++ random/ppo/ur5_reacher.py
@@ -3,6 +3,7 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
+
 from __future__ import print_function
 import time
 import copy
