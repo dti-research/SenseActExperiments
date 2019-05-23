a8f74e71948f73e1640d9a5fec6e8ad5402a9664
dti <Nicolai Anton Lynnerup>
2019-05-23 06:06:29 +0200

    Removed reset UR from bash script

--- code/run_top5_params_x10_trpo.sh
+++ code/run_top5_params_x10_trpo.sh
@@ -56,13 +56,6 @@ do
 
     for j in {0..9} # number of tests for each hyperparameter configuration
     do
-        
-        echo "***********************************"
-        echo "* RESTARTING ROBOT"
-        echo "* Stand Clear!"
-        echo "***********************************"
-        python3 restart_ur.py
-
         echo " - Running test #$j for hyperparameter configuration $filename"
         python3 train.py -f $filename
     done
