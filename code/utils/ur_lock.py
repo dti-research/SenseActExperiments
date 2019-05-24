60d8de07f46a7fe54e5be2b383ac4b4c0e2b2de8
dti <Nicolai Anton Lynnerup>
2019-05-24 14:00:22 +0200

    Added script to lock UR Teach Pendant

---
+++ code/utils/ur_lock.py
@@ -0,0 +1,10 @@
+import time
+import socket
+
+if __name__ == "__main__":
+    # Connect to the robot's dashboard interface and save the log
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('192.168.1.100',29999))
+
+    s.send("setUserRole locked\n".encode('ascii'))
+    s.close()
