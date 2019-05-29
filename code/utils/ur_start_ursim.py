7e4925424848b254bf0221b5e53435021805b6a6
Nicolai Anton Lynnerup <nily@teknologisk.dk>
2019-05-29 16:04:10 +0200

    Added scripts to send commands to the UR and remote start UR sim

---
+++ code/utils/ur_start_ursim.py
@@ -0,0 +1,32 @@
+import time
+import paramiko
+import argparse
+
+# Setup argparser
+parser = argparse.ArgumentParser(description='Kills the URControl process via SSH',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--robot-ip",
+                    help="robot ip",
+                    metavar="192.168.1.100",
+                    required=True)
+parser.add_argument('--username', dest='username',
+                    default="root",
+                    type=str,
+                    help='name of the user at the robot')
+
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    # Connect to the robot's dashboard interface and kill the URControl process
+    p = paramiko.SSHClient()
+    p.set_missing_host_key_policy(paramiko.AutoAddPolicy())   # This script doesn't work for me unless this line is added!
+    p.connect(args.robot_ip, username=args.username, password="easybot")
+    stdin, stdout, stderr = p.exec_command("export DISPLAY=:0")
+    opt = stdout.readlines()
+    opt = "".join(opt)
+    print(opt)
+    stdin, stdout, stderr = p.exec_command("nohup bash ursim-current/start-ursim.sh UR5")
+    opt = stdout.readlines()
+    opt = "".join(opt)
+    print(opt)
+    time.sleep(2)
